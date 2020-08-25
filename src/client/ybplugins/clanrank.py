db4c355d28a26eefd147a435023b908c355ff24d
uroxic <uroxic@gmail.com>
2020-08-25 09:41:26 +0800

    Update clanrank.py

--- src/client/ybplugins/clanrank.py
+++ src/client/ybplugins/clanrank.py
@@ -16,6 +16,47 @@ from quart import Quart
 from random import randint
 
 
+bossData = {
+    'scoreRate': [
+        [1, 1, 1.1, 1.1, 1.2],
+        [1.2, 1.2, 1.5, 1.7, 2],
+    ],
+    'hp': [6000000, 8000000, 10000000, 12000000, 20000000],
+    'max': 2,
+}
+
+
+def calc_hp(hp_base: int):
+    zm = 1
+    king = 1
+    cc = 0.0
+    remain = 0.0
+    damage = 0
+    remainHp = 0.0
+    remainPer = 0.0
+
+    while True:
+        nowZm = bossData['max'] - 1 if zm > bossData['max'] else zm - 1
+        cc += bossData['scoreRate'][nowZm][king - 1] * bossData['hp'][king - 1]
+        if cc > hp_base:
+            cc -= bossData['scoreRate'][nowZm][king - 1] * \
+                bossData['hp'][king - 1]
+            remain = (hp_base - cc) / bossData['scoreRate'][nowZm][king - 1]
+            damage += remain
+            remainPer = 1.0 - remain / bossData['hp'][king - 1]
+            remainHp = bossData['hp'][king - 1] - remain
+            break
+        damage += bossData['hp'][king - 1]
+        if king == 5:
+            zm += 1
+            king = 1
+            continue
+        king += 1
+    remainPer *= 100
+    bdk = bossData['hp'][king - 1]
+    return f'{zm}周目{king}王 [{math.floor(remainHp)}/{bdk}]  {round(remainPer, 2)}%'
+
+
 class Clanrank:
     def __init__(self,
                  glo_setting: Dict[str, Any],
@@ -145,6 +186,8 @@ class Clanrank:
                         msg += str(i['rank'])
                         msg += '\n分数 '
                         msg += str(i['damage'])
+                        msg += '\n进度 '
+                        msg += str(calc_hp(int(i['damage'])))
                         msg += '\n会长 '
                         msg += str(i['leader_name'])
                         msg += '\n\n'
@@ -176,6 +219,8 @@ class Clanrank:
                 msg += str(i['damage']).ljust(10, ' ')
                 msg += ' 排名'
                 msg += str(i['rank'])
+                msg += '\n进度 '
+                msg += str(calc_hp(int(i['damage'])))
                 msg += '\n'
             msg += '\n'
             msg += self.time[str(rtime)] if str(rtime) in self.time else '最新数据'
@@ -222,6 +267,8 @@ class Clanrank:
                 msg += str(i['rank'])
                 msg += '\n分数 '
                 msg += str(i['damage'])
+                msg += '\n进度 '
+                msg += str(calc_hp(int(i['damage'])))
                 msg += '\n会长 '
                 msg += str(i['leader_name'])
                 msg += '\n\n'
@@ -253,6 +300,8 @@ class Clanrank:
                 msg += str(i['rank'])
                 msg += '\n分数 '
                 msg += str(i['damage'])
+                msg += '\n进度 '
+                msg += str(calc_hp(int(i['damage'])))
                 msg += '\n会长 '
                 msg += str(i['leader_name'])
                 msg += '\n\n'
@@ -285,6 +334,8 @@ class Clanrank:
                 msg += str(i['rank'])
                 msg += '\n分数 '
                 msg += str(i['damage'])
+                msg += '\n进度 '
+                msg += str(calc_hp(int(i['damage'])))
                 msg += '\n会长 '
                 msg += str(i['leader_name'])
                 msg += '\n\n'
@@ -317,6 +368,8 @@ class Clanrank:
                 msg += str(i['rank'])
                 msg += '\n分数 '
                 msg += str(i['damage'])
+                msg += '\n进度 '
+                msg += str(calc_hp(int(i['damage'])))
                 msg += '\n会长 '
                 msg += str(i['leader_name'])
                 msg += '\n\n'
